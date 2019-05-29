45b89e6685c0dc947af4009a6ea0d641ca93c131
github username <cristi.nord@gmail.com>
2019-05-29 20:46:53 +0100

    index

--- src/App.js
+++ src/App.js
@@ -3,27 +3,31 @@ import React from "react";
 function App() {
   let items = [
     {
+      id: 1,
       text: "Learn Javascript",
       completed: false
     },
     {
+      id: 2,
       text: "Learn React",
       completed: false
     },
     {
+      id: 3,
       text: "Build a React App",
       completed: false
     }
   ];
+  let title = "Things to do";
 
   return (
     <div className="container">
       <div className="row">
         <div className="todolist">
-          <h1>Todos</h1>
+          <h1>{title.toUpperCase()}</h1>
           <ul className="list-unstyled">
             {items.map(item => (
-              <li className="ui-state-default">
+              <li key={item.id} className="ui-state-default">
                 <div className="checkbox">
                   <label>
                     <input type="checkbox" value="" />
