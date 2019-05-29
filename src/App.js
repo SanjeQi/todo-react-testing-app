7488073aef96b443b2a8b5cc664da6ef73ed297f
github username <cristi.nord@gmail.com>
2019-05-29 20:30:13 +0100

    bootstrap jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import React from "react";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="todolist">
+          <h1>Todos</h1>
+          <ul className="list-unstyled">
+            <li className="ui-state-default">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" value="" />
+                  Take out the trash
+                </label>
+              </div>
+            </li>
+            <li className="ui-state-default">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" value="" />
+                  Buy bread
+                </label>
+              </div>
+            </li>
+            <li className="ui-state-default">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" value="" />
+                  Teach penguins to fly
+                </label>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// Add bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
+
+// Add our style
+import "./style/index.css";
+
 ReactDOM.render(<App />, document.getElementById("root"));
