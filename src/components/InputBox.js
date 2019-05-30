3d1df3170816deab9e8976227dfbbb8afb7c55b1
github username <cristi.nord@gmail.com>
2019-05-30 17:54:53 +0100

    components

---
+++ src/components/InputBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function InputBox() {
+  return (
+    <input
+      type="text"
+      className="form-control add-todo"
+      placeholder="Add New"
+    />
+  );
+}
