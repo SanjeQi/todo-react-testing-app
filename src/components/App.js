eed764d58efb5c0a9a45301b37dc656b5932baa7
github username <cristi.nord@gmail.com>
2019-05-31 12:24:18 +0100

    comp_2

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import TodoList from "./TodoList";
+import { FILTER_ACTIVE } from "./../services/filter";
+import { getAll, createNew, updateStatus } from "./../services/todo";
 
-function App() {
-  let items = [
-    {
-      id: 1,
-      text: "Learn Javascript",
-      completed: false
-    },
-    {
-      id: 2,
-      text: "Learn React",
-      completed: false
-    },
-    {
-      id: 3,
-      text: "Build a React App",
-      completed: false
-    }
-  ];
-  let title = "Things to do";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filter: FILTER_ACTIVE,
+      items: getAll()
+    };
+  }
 
-  return (
-    <div className="container">
-      <div className="row">
-        <TodoList title={title} items={items} />
+  render() {
+    let title = "Things to do";
+
+    return (
+      <div className="container">
+        <div className="row">
+          <TodoList
+            title={title}
+            addNew={this.addNew.bind(this)}
+            changeFilter={this.changeFilter.bind(this)}
+            changeStatus={this.changeStatus.bind(this)}
+            {...this.state}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  addNew(text) {
+    let item = createNew(text);
+    let updatedList = this.state.items.concat([item]);
+
+    this.setState({ items: updatedList });
+  }
+
+  changeFilter(filter) {
+    this.setState({ filter });
+  }
+
+  changeStatus(itemId, completed) {
+    const updatedList = updateStatus(this.state.items, itemId, completed);
+
+    this.setState({ items: updatedList });
+  }
 }
 
 export default App;
