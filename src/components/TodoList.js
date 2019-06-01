340ddde8a7c4fb74241c343e5cd71f3a0b13259f
github username <cristi.nord@gmail.com>
2019-06-01 13:45:02 +0100

    comp_3

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,21 +2,26 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import TodoItem from "./TodoItem";
+import { applyFilter } from "./../services/filter";
 
 function TodoList(props) {
-  const { title, items } = props;
+  const { title, items, addNew, filter, changeFilter, changeStatus } = props;
   const count = items.length;
+  const filteredList = applyFilter(items, filter);
 
   return (
     <div className="todolist">
-      <Header title={title} />
-
-      <ul className="list-unstyled">
-        {items.map(item => (
-          <TodoItem key={item.id} data={item} />
-        ))}
-      </ul>
-      <Footer count={count} />
+      <Header title={title} addNew={addNew} />
+      {filteredList.length > 0 ? (
+        <ul className="list-unstyled">
+          {filteredList.map(item => (
+            <TodoItem key={item.id} data={item} changeStatus={changeStatus} />
+          ))}
+        </ul>
+      ) : (
+        <p className="alert alert-info">There are no items.</p>
+      )}
+      <Footer {...{ count, filter, changeFilter }} />
     </div>
   );
 }
