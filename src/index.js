303935fdedbe9219ad1071857acc0871cc62ce8c
github username <cristi.nord@gmail.com>
2019-06-05 16:28:21 +0100

    style and filter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
 
 // Add bootstrap
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/css/bootstrap-theme.css";
 
 // Add our style
 import "./style/index.css";
