2b4a382b1058dc5c0c14d4b0381ae9afab74d025
github username <cristi.nord@gmail.com>
2019-06-20 23:04:19 +0100

    Info component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Info from "./Info";
 import Header from "./Header";
 import Footer from "./Footer";
 import FilteredList from "./FilteredList";
@@ -21,6 +22,7 @@ export default function TodoList(props) {
       <Header {...{ addNew, mode, query, setSearchQuery }} />
       <FilteredList {...{ items, changeStatus }} />
       <Footer {...{ count, filter, changeFilter, mode, changeMode }} />
+      <Info {...{ mode }} />
     </div>
   );
 }
